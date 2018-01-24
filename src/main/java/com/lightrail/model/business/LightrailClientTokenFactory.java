d3efc8d91e9ec379540f408831d7393870178657
jeff <jeff@giftbit.com>
2018-01-24 13:41:23 -0800

    Use javax.xml.bind.DatatypeConverter instead of the deprecared and then removed sun.misc.BASE64Decoder.

--- src/main/java/com/lightrail/model/business/LightrailClientTokenFactory.java
+++ src/main/java/com/lightrail/model/business/LightrailClientTokenFactory.java
@@ -7,34 +7,35 @@ import com.lightrail.model.Lightrail;
 import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import sun.misc.BASE64Decoder;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import javax.xml.bind.DatatypeConverter;
 
 public class LightrailClientTokenFactory {
 
     public static String generate(String shopperId, Integer validitySeconds) throws IOException {
-
         JwtBuilder builder = Jwts.builder();
         String apiKey = Lightrail.apiKey;
         String secret = Lightrail.clientSecret;
 
-        if (apiKey == null)
+        if (apiKey == null) {
             throw new BadParameterException("Lightrail.apiKey is not set.");
-        if (secret == null)
+        }
+        if (secret == null) {
             throw new BadParameterException("Lightrail.clientSecret is not set.");
+        }
 
         String payload = apiKey.substring(apiKey.indexOf(".") + 1);
         payload = payload.substring(0, payload.indexOf("."));
 
-        payload = new String(new BASE64Decoder().decodeBuffer(payload));
+        payload = new String(DatatypeConverter.parseBase64Binary(payload), "UTF-8");
         JsonObject jsonObject = new Gson().fromJson(payload, JsonObject.class);
         String gui = jsonObject.get("g").getAsJsonObject().get("gui").getAsString();
 
         Map<String, Object> claims = new HashMap<String, Object>();
-        Long iat = System.currentTimeMillis()/1000;
+        Long iat = System.currentTimeMillis() / 1000;
         claims.put("iat", iat);
         claims.put("shopperId", shopperId);
 
@@ -51,6 +52,4 @@ public class LightrailClientTokenFactory {
                 .signWith(SignatureAlgorithm.HS256, secret.getBytes("UTF-8"))
                 .compact();
     }
-
-
 }
