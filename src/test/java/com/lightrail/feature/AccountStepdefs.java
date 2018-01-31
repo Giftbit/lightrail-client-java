0a0f34286603457f0cc1b73a54f9db57ed002128
Tana Jukes <tana.j@lightrail.com>
2018-01-31 10:18:23 -0800

    More wiring
    Improved param names, splitting params and getting actual data from variables.json.

--- src/test/java/com/lightrail/feature/AccountStepdefs.java
+++ src/test/java/com/lightrail/feature/AccountStepdefs.java
@@ -1,19 +1,49 @@
 package com.lightrail.feature;
 
 import com.google.gson.*;
+import com.google.gson.JsonObject;
 import cucumber.api.java.en.Given;
 
 import java.io.FileReader;
+import java.util.*;
 
 
 public class AccountStepdefs {
     @Given("^a contact .*\\s*exists?\\s*.*: requires minimum parameters \\[(.[^\\]]+)\\] and makes the following REST requests: \\[(.[^\\]]+)\\](?: and throws the following error: \\[(.[^\\]]+)\\])?$")
-    public void contact_does_or_does_not_exist(String minParams, String expectedRequestsAndResponses, String errorName) throws Throwable {
-        System.out.println(minParams);
-        System.out.println(expectedRequestsAndResponses);
-        System.out.println(errorName);
-
+    public void contact_does_or_does_not_exist(String minimumParams, String expectedRequestsAndResponses, String errorName) throws Throwable {
         JsonObject jsonVariables = new JsonParser().parse(new FileReader("src/test/java/com/lightrail/feature/variables.json")).getAsJsonObject();
-        System.out.println(jsonVariables.get("shopperId"));
+
+        String[] minParamKeys = minimumParams.split(", ");
+        Map<String, JsonElement> minParams = new HashMap<>();
+        for (int index = 0; index < minParamKeys.length; index++) {
+            minParams.put(minParamKeys[index], jsonVariables.get(minParamKeys[index]));
+        }
+
+        String[] reqResKeys = expectedRequestsAndResponses.split(", ");
+        Map<String, JsonElement> reqResCollection = new HashMap<>();
+        for (int index = 0; index < reqResKeys.length; index++) {
+            reqResCollection.put(reqResKeys[index], jsonVariables.get("requestResponseCombos").getAsJsonObject().get(reqResKeys[index]));
+        }
+
+
+        System.out.println("LOOPING OVER REQRES:");
+
+
+        for (String name : reqResCollection.keySet()) {
+            String reqResKey = name.toString();
+            JsonObject reqResDetails = reqResCollection.get(reqResKey).getAsJsonObject();
+
+//            if /error/ regex matches reqResKey
+//              expect to make call and receive error:
+//                  method: reqResDetails.get("httpMethod")
+//                  endpoint: reqResDetails.get("endpoint")
+//                  reqResDetails: reqResDetails.get("httpMethod")
+//                  errorName: errorName
+//            else
+//              expect to make call and receive response
+//            end
+
+            System.out.println("method: " + reqResDetails.get("httpMethod") + " endpoint: " + reqResDetails.get("endpoint") + " response: " + reqResDetails.get("response"));
+        }
     }
 }
