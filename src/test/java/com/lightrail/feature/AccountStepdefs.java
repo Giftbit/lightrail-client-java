f0650d7022f8e0d29cfd7807489da209acc497f4
Tana Jukes <tana.j@lightrail.com>
2018-02-02 13:06:45 -0800

    Move feature file & variables to resources dir

--- src/test/java/com/lightrail/feature/AccountStepdefs.java
+++ src/test/java/com/lightrail/feature/AccountStepdefs.java
@@ -19,7 +19,7 @@ import static org.mockito.Mockito.*;
 public class AccountStepdefs {
     @Given("^a contact .*\\s*exists?\\s*.*: requires minimum parameters \\[(.[^\\]]+)\\] and makes the following REST requests: \\[(.[^\\]]+)\\](?: and throws the following error: \\[(.[^\\]]+)\\])?$")
     public void contact_does_or_does_not_exist(String minimumParams, String expectedRequestsAndResponses, String errorName) throws Throwable {
-        JsonObject jsonVariables = new JsonParser().parse(new FileReader("src/test/java/com/lightrail/feature/variables.json")).getAsJsonObject();
+        JsonObject jsonVariables = new JsonParser().parse(new FileReader("src/test/resources/variables.json")).getAsJsonObject();
 
         String[] minParamKeys = minimumParams.split(", ");
         Map<String, JsonElement> minParams = new HashMap<>();
