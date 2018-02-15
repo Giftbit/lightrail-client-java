5c3b03289882f6ad85553bed1d1baa82bae5ab3a
Tana Jukes <tana.j@lightrail.com>
2018-02-15 09:55:30 -0800

    Refactor only: extract segments of test logic to helper functions

--- src/test/java/com/lightrail/feature/AccountStepdefs.java
+++ src/test/java/com/lightrail/feature/AccountStepdefs.java
@@ -4,7 +4,9 @@ import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.lightrail.exceptions.AuthorizationException;
 import com.lightrail.exceptions.CouldNotFindObjectException;
+import com.lightrail.exceptions.InsufficientValueException;
 import com.lightrail.model.Lightrail;
 import com.lightrail.model.api.net.APICore;
 import com.lightrail.model.api.net.DefaultNetworkProvider;
@@ -14,6 +16,7 @@ import com.lightrail.model.business.AccountCard;
 import cucumber.api.java.en.Given;
 
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Pattern;
@@ -30,23 +33,45 @@ public class AccountStepdefs {
         Lightrail.apiKey = "123";
         JsonObject jsonVariables = new JsonParser().parse(new FileReader("src/test/resources/variables.json")).getAsJsonObject();
 
+        RequestParamsCreateAccountByContactId minParams = getContactIdParams(minimumParams, jsonVariables);
+        Map<String, JsonElement> reqResCollection = getReqResCollection(expectedRequestsAndResponses, jsonVariables);
+
+        NetworkProvider npMock = mock(DefaultNetworkProvider.class);//, new DefaultJsonAnswer());
+        APICore.setNetworkProvider(npMock);
+
+        setReqResExpectations(reqResCollection, npMock);
+
+        try {
+            AccountCard.create(minParams);
+        } catch (CouldNotFindObjectException e) {
+        }
+
+        verifyMock(reqResCollection, npMock);
+    }
+
+
+    private RequestParamsCreateAccountByContactId getContactIdParams(String minimumParams, JsonObject jsonVariables) {
         String[] minParamKeys = minimumParams.split(", ");
         JsonObject miniParams = new JsonObject();
         for (int index = 0; index < minParamKeys.length; index++) {
             miniParams.add(minParamKeys[index], jsonVariables.get(minParamKeys[index]));
         }
-        RequestParamsCreateAccountByContactId minParams = new RequestParamsCreateAccountByContactId(new Gson().toJson(miniParams));
 
+        RequestParamsCreateAccountByContactId minParams = new RequestParamsCreateAccountByContactId(new Gson().toJson(miniParams));
+        return minParams;
+    }
+
+    private Map<String, JsonElement> getReqResCollection(String expectedRequestsAndResponses, JsonObject jsonVariables) {
         String[] reqResKeys = expectedRequestsAndResponses.split(", ");
         Map<String, JsonElement> reqResCollection = new HashMap<>();
         for (int index = 0; index < reqResKeys.length; index++) {
             reqResCollection.put(reqResKeys[index], jsonVariables.get("requestResponseCombos").getAsJsonObject().get(reqResKeys[index]));
         }
 
-        NetworkProvider npMock = mock(DefaultNetworkProvider.class);//, new DefaultJsonAnswer());
-        APICore.setNetworkProvider(npMock);
+        return reqResCollection;
+    }
 
-
+    private void setReqResExpectations(Map<String, JsonElement> reqResCollection, NetworkProvider npMock) throws AuthorizationException, CouldNotFindObjectException, InsufficientValueException, IOException {
         for (String name : reqResCollection.keySet()) {
             String reqResKey = name.toString();
             JsonObject reqResDetails = reqResCollection.get(reqResKey).getAsJsonObject();
@@ -55,27 +80,23 @@ public class AccountStepdefs {
             String method = reqResDetails.get("httpMethod").getAsString();
             String response = reqResDetails.get("response").toString();
 
+            // todo: generate body string?
+
             if (Pattern.compile("(?i)error").matcher(reqResKey).find()) {
                 when(npMock.getRawAPIResponse(contains(endpoint), matches("(?i)" + method), (String) any())).thenThrow(new CouldNotFindObjectException(""));  // todo: this exception needs to match the `errorName` passed in from the feature file
             } else {
                 when(npMock.getRawAPIResponse(contains(endpoint), matches("(?i)" + method), (String) any())).thenReturn(reqResDetails.get("response").toString());
             }
         }
+    }
 
-        // todo: generate body string?
-
-        try {
-            AccountCard.create(minParams);
-        } catch (CouldNotFindObjectException e) {
-        }
-
+    private void verifyMock(Map<String, JsonElement> reqResCollection, NetworkProvider npMock) throws AuthorizationException, CouldNotFindObjectException, InsufficientValueException, IOException {
         for (String name : reqResCollection.keySet()) {
             String reqResKey = name.toString();
             JsonObject reqResDetails = reqResCollection.get(reqResKey).getAsJsonObject();
 
             String endpoint = reqResDetails.get("endpoint").getAsString();
             String method = reqResDetails.get("httpMethod").getAsString();
-
 
             verify(npMock, times(1)).getRawAPIResponse(contains(endpoint), matches("(?i)" + method), (String) any());
         }
