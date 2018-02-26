4c586d6e6260cdc3ce88bdcf492fed60c8d58f1d
Tana Jukes <tana.j@lightrail.com>
2018-02-26 10:40:55 -0800

    Update Contact constructors
    Similar changes as to 'Card' in prev commit

--- src/main/java/com/lightrail/model/api/objects/Contact.java
+++ src/main/java/com/lightrail/model/api/objects/Contact.java
@@ -1,5 +1,9 @@
 package com.lightrail.model.api.objects;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.lightrail.exceptions.BadParameterException;
+
 @JsonObjectRoot("contact")
 public class Contact extends LightrailObject {
     public String contactId;
@@ -34,6 +38,17 @@ public class Contact extends LightrailObject {
     }
 
     public Contact(String jsonObject) {
-        super(jsonObject);
+        JsonObject jsonContact = new Gson().fromJson(jsonObject, JsonObject.class);
+        Contact contact = null;
+        if ((jsonContact.get("contact") != null)) {
+            String nestedContact = jsonContact.get("contact").getAsString();
+            new Gson().fromJson(nestedContact, Contact.class);
+        } else if (jsonContact.get("contactId") != null) {
+            new Gson().fromJson(jsonObject, Contact.class);
+        } else {
+            throw new BadParameterException("Could not create contact from JSON: " + jsonObject);
+        }
     }
+
+    public Contact() {}
 }
