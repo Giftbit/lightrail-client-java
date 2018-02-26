9fb9322e8875b014960a4a25f5961301e9d54ea5
Tana Jukes <tana.j@lightrail.com>
2018-02-26 11:06:27 -0800

    Update LightrailCard constructors
    Similar to previous constructor updates

--- src/main/java/com/lightrail/model/business/LightrailCard.java
+++ src/main/java/com/lightrail/model/business/LightrailCard.java
@@ -1,5 +1,6 @@
 package com.lightrail.model.business;
 
+import com.google.gson.Gson;
 import com.lightrail.exceptions.AuthorizationException;
 import com.lightrail.exceptions.BadParameterException;
 import com.lightrail.exceptions.CouldNotFindObjectException;
@@ -11,19 +12,22 @@ import java.io.IOException;
 
 class LightrailCard extends Card {
 
+    public LightrailCard() {
+    }
+
     public LightrailCard(String jsonObject) {
-        super(jsonObject);
+        new Gson().fromJson(jsonObject, LightrailCard.class);
     }
 
     public LightrailCard(Card card) {
-        super(card.getRawJson());
+        new Gson().fromJson(new Gson().toJson(card), LightrailCard.class);
     }
 
-    public CardDetails retrieveCardDetails () throws AuthorizationException, CouldNotFindObjectException, IOException {
+    public CardDetails retrieveCardDetails() throws AuthorizationException, CouldNotFindObjectException, IOException {
         return APICore.Cards.retrieveDetailsByCardId(this.cardId);
     }
 
-    public int retrieveMaximumValue () throws IOException, AuthorizationException, CouldNotFindObjectException {
+    public int retrieveMaximumValue() throws IOException, AuthorizationException, CouldNotFindObjectException {
         CardDetails cardDetails = retrieveCardDetails();
         int maximumValue = 0;
         for (ValueStore valueStore : cardDetails.getValueStores()) {
