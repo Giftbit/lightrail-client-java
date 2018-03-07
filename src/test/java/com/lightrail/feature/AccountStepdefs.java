7bfb250e3c977dd3f01fdf4613a3d7ee0461612f
Tana Jukes <tana.j@lightrail.com>
2018-03-07 11:08:36 -0800

    Initial Cucumber tests for Accounts
    Stepdef logic copied and updated from previous version. Only the tests tagged '@only_this_one' work so far.

---
+++ src/test/java/com/lightrail/feature/AccountStepdefs.java
@@ -0,0 +1,109 @@
+package com.lightrail.feature;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.lightrail.LightrailClient;
+import com.lightrail.model.LightrailException;
+import com.lightrail.network.DefaultNetworkProvider;
+import com.lightrail.network.NetworkProvider;
+import com.lightrail.params.CreateAccountCardByContactIdParams;
+import com.lightrail.params.CreateAccountCardByShopperIdParams;
+import cucumber.api.java.en.Given;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.contains;
+import static org.mockito.ArgumentMatchers.matches;
+import static org.mockito.Mockito.*;
+
+public class AccountStepdefs {
+    @Given("^a contact .*\\s*exists?\\s*.*: requires minimum parameters \\[(.[^\\]]+)\\] and makes the following REST requests: \\[(.[^\\]]+)\\](?: and throws the following error: \\[(.[^\\]]+)\\])?$")
+    public void contact_does_or_does_not_exist(String minimumParams, String expectedRequestsAndResponses, String errorName) throws Throwable {
+
+        JsonObject jsonVariables = new JsonParser().parse(new FileReader("src/test/resources/variables.json")).getAsJsonObject();
+
+        Map<String, JsonElement> reqResCollection = getReqResCollection(expectedRequestsAndResponses, jsonVariables);
+
+        NetworkProvider npMock = mock(DefaultNetworkProvider.class);
+        LightrailClient lr = new LightrailClient("123", "123", npMock);
+
+        setReqResExpectations(reqResCollection, lr);
+
+        if (Pattern.compile("(?i)contactid").matcher(minimumParams).find()) {
+            JsonObject jsonParams = getJsonParams(minimumParams, jsonVariables);
+            CreateAccountCardByContactIdParams minParams = lr.gson.fromJson(jsonParams.toString(), CreateAccountCardByContactIdParams.class);
+            boolean exceptionThrown = false;
+
+            lr.accounts.create(minParams);
+
+            if (errorName != null) {
+                assertEquals(true, exceptionThrown);
+            }
+        } else if (Pattern.compile("(?i)shopperid").matcher(minimumParams).find()) {
+            JsonObject jsonParams = getJsonParams(minimumParams, jsonVariables);
+            CreateAccountCardByShopperIdParams minParams = lr.gson.fromJson(jsonParams.toString(), CreateAccountCardByShopperIdParams.class);
+            lr.accounts.create(minParams);
+        }
+
+        verifyMock(reqResCollection, lr);
+    }
+
+
+    private JsonObject getJsonParams(String minimumParams, JsonObject jsonVariables) {
+        String[] minParamKeys = minimumParams.split(", ");
+        JsonObject miniParams = new JsonObject();
+        for (int index = 0; index < minParamKeys.length; index++) {
+            miniParams.add(minParamKeys[index], jsonVariables.get(minParamKeys[index]));
+        }
+        return miniParams;
+    }
+
+    private Map<String, JsonElement> getReqResCollection(String expectedRequestsAndResponses, JsonObject jsonVariables) {
+        String[] reqResKeys = expectedRequestsAndResponses.split(", ");
+        Map<String, JsonElement> reqResCollection = new HashMap<>();
+        for (int index = 0; index < reqResKeys.length; index++) {
+            reqResCollection.put(reqResKeys[index], jsonVariables.get("requestResponseCombos").getAsJsonObject().get(reqResKeys[index]));
+        }
+
+        return reqResCollection;
+    }
+
+    private void setReqResExpectations(Map<String, JsonElement> reqResCollection, LightrailClient lr) throws IOException, LightrailException {
+        for (String name : reqResCollection.keySet()) {
+            String reqResKey = name;
+            JsonObject reqResDetails = reqResCollection.get(reqResKey).getAsJsonObject();
+
+            String endpoint = reqResDetails.get("endpoint").getAsString();
+            String method = reqResDetails.get("httpMethod").getAsString();
+            String response = reqResDetails.get("response").toString();
+
+            // todo: generate body string?
+
+            if (Pattern.compile("(?i)error").matcher(reqResKey).find()) {
+                when(lr.networkProvider.getAPIResponse(matches(lr.apiKey), contains(endpoint), matches("(?i)" + method), (String) any())).thenThrow(new LightrailException(""));  // todo: this exception needs to match the `errorName` passed in from the feature file
+            } else {
+                when(lr.networkProvider.getAPIResponse(matches(lr.apiKey), contains(endpoint), matches("(?i)" + method), (String) any())).thenReturn(reqResDetails.get("response").toString());
+            }
+        }
+    }
+
+    private void verifyMock(Map<String, JsonElement> reqResCollection, LightrailClient lr) throws IOException, LightrailException {
+        for (String name : reqResCollection.keySet()) {
+            String reqResKey = name.toString();
+            JsonObject reqResDetails = reqResCollection.get(reqResKey).getAsJsonObject();
+
+            String endpoint = reqResDetails.get("endpoint").getAsString();
+            String method = reqResDetails.get("httpMethod").getAsString();
+
+            verify(lr.networkProvider, times(1)).getAPIResponse(matches(lr.apiKey), contains(endpoint), matches("(?i)" + method), (String) any());
+        }
+    }
+
+}
--- src/test/resources/accounts.feature
+++ src/test/resources/accounts.feature
@@ -1,13 +1,8 @@
 Feature: Account Card
 
-# NOTE These are tags. Scenarios are tagged individually by adding these right above the #Scenario def like this:
   @account_creation @by_shopper_id
 
   Scenario: Create by shopperId
-  # NOTE Each of these lines (steps) can start with any of the keywords (Given, When, Then, And, But or *). They're completely interchangeable.
-  # NOTE Parameters passed to the stepdef are enclosed in [] for simplicity; any other delimiter could be used if the stepdef's regex is updated. Params listed correspond to keys in variables.json which is read by the stepdef file to actually access the data.
-  # NOTE All of these are formatted to match the same regex in account_stepdefs.rb. Adding/changing steps may require the regex to be updated.
-  # NOTE Consider in the future: other tests (contact creation, etc) could easily be formatted the same way. Consider adding a feature-identifying keyword to each step: eg, `When ACCOUNT CREATION a contact exists [...]`
     When a contact exists but has no account: requires minimum parameters [shopperId, currency, userSuppliedId] and makes the following REST requests: [contactsSearchOneResult, accountCardSearchNoResults, accountCardCreate]
 
     When a contact exists and has an account: requires minimum parameters [shopperId, currency, userSuppliedId] and makes the following REST requests: [contactsSearchOneResult, accountCardSearchOneResult]
@@ -15,7 +10,7 @@ Feature: Account Card
     When a contact doesn't exist: requires minimum parameters [shopperId, currency, userSuppliedId] and makes the following REST requests: [contactsSearchNoResults, contactCreate, accountCardSearchNoResults, accountCardCreate]
 
 
-  @account_creation @by_contact_id
+  @account_creation @by_contact_id @only_this_one
 
   Scenario: Create by contactId
     When a contact exists but has no account: requires minimum parameters [contactId, currency, userSuppliedId] and makes the following REST requests: [contactGet, accountCardSearchNoResults, accountCardCreate]
